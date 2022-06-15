403a7b8c39fc5a773dda7970202d8b3dc8065f48
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-06-15 23:00:03 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(event) {
+     event.preventDefault();
+    const myName = document.getElementById("nameField")
+    const myEmail =document.getElementById("emailField")
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers :{
+            "Content-Type": "application/json",
+            Accept : "application/json"
+        },
+        body: JSON.stringify({
+           name: myName.value,
+           email: myEmail.value     
+        })
+    }).then((response)=>{
+        response.json();
+        return response.json;
+    }).then((responseData)=>{
+      const h2 = document.querySelector(".title#name h3");
+      h2.textContent= responseData.name;
+
+      const h4 = document.querySelector(".response#email h4");
+      h4.textContent = responseData.email;
+
+      const myId = document.querySelector(".id#myIdNo h4")
+      //myId = data.id
+    })
+    
+}
+//document.querySelector("form").addEventListener("submit", submitData)
